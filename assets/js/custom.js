5bea27a050fbe2f3c45dc45b55041303d57997dd
Aquib <hossainaquib20@gmail.com>
2022-09-13 18:29:03 +0600

    3rd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,7 @@
 $(function() {
+    "use strict";
 
+    //   date picker
     rome(input_from, {
         dateValidator: rome.val.beforeEq(input_to),
         time: false
@@ -23,6 +25,66 @@ $(function() {
       window.onload= function () {
           sorting.parentElement.setAttribute('class', sortingClass);
       }
+
+
+
+
+
+
+
+
+
+      
+    // range sldier 
+    function Slider(slider) {
+      
+        this.slider = slider;
+        slider.addEventListener('input', function() {
+          this.updateSliderOutput();
+          this.updateSliderLevel();
+          this.updateBars();
+        }.bind(this), false);
+        
+        this.level = function() {
+          // actual slider
+          let level = this.slider.querySelector('.slider-input');
+          return level.value;
+        }
+        
+        this.levelString = function() {
+          return parseInt(this.level());
+        }
+        
+        this.updateSliderOutput = function() {
+          // number indicator below slider
+          let output = this.slider.querySelector('.slider-output');
+          // slider circle handle
+          let thumb = this.slider.querySelector('.slider-thumb');
+          output.value = "$"+this.levelString();
+          output.style.left = this.levelString() + '%';
+          thumb.style.left = this.levelString() + '%';
+        }
+        
+        this.updateSliderLevel = function() {
+          // dark gray part of slider
+          let level = this.slider.querySelector('.slider-level');
+          level.style.width = this.levelString() + '%';
+        }
+        
+        this.updateBars = function() {
+          // actual slider
+          let level = this.slider.querySelector('.slider-input');
+          let analogousLevel = level.value;
+          let inverseLevel = 100 - level.value;
+          $('.fill_analogous').css('width', analogousLevel + '%');
+          $('.fill_inverse').css('width', inverseLevel + '%');
+        }
+        
+      }
+      
+      let volumeSlider = document.getElementById('volume');
+      new Slider(volumeSlider);
+    
   });
 
 
